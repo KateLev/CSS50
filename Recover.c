9d60f494ecf1c52c17b2ff24790cfe3ec06f99de
KateLev <onlyforreg3@yandex.ru>
2019-10-14 01:00:22 +0400

    I understood how to make a normal array with malloc

--- Recover.c
+++ Recover.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     int n = 0;
+    int segment_count = 0;
     char *infile = argv[1]; //argv[1] is a name of camera's card
     FILE *inptr = fopen(infile, "r"); //we are opening the file. In this case it's card.raw
 
@@ -16,27 +17,24 @@ int main(int argc, char *argv[])
     }
 
     //we need a new char for each 512
-    char *container [512];
-    int buffer[512];
+    unsigned char* buffer=malloc(512);
 
     //we are starting to read infile and store information in container
-    while (fread(container, 1, 512, inptr)) // because 1 block of 512 and when we finished there will no any blocks
+    while (fread(buffer, 1, 512, inptr)) // because 1 block of 512 and when we finished there will no any blocks
     {
-        for (int i = 0; i < 3; i++)
-        {
-            buffer[i] = atof (container[i]);
 
-        }
+        segment_count+=1;
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3]&0xf0) == 0xe0 )
         {
             n+=1;
         }
 
+
     }
     printf("We have %i jpeg files \n", n);
-
+    printf("We have %i 512 blocks \n", segment_count);
     // close infile
     fclose(inptr);
-    free (container);
-    free (buffer);
+    free(buffer);
+
 }
